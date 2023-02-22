d3d2edb998310bc54c6127e1ace0525b6ac3bb83
es222 <esra.sogut88@gmail.com>
2023-02-22 18:46:21 +0300

    MobilUygulamalar
    MobilUygulamalar

---
+++ Uyg_Hayvanlar_Alemi/app/src/main/java/com/example/uyg_hayvanlar_alemi/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.uyg_hayvanlar_alemi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
